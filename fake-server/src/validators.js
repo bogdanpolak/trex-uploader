e60f7c7b4391e809356ee6ee205ca484a68d0114
Bogdan Polak <bogdanpol@gmail.com>
2020-10-12 23:50:32 +0200

    Done ndcValidator

--- fake-server/src/validators.js
+++ fake-server/src/validators.js
@@ -6,6 +6,7 @@ const validaton = {
 
 const ok = () =>  ({result: validaton.OK});
 const error = (msg) => ({result: validaton.ERROR, message: msg});
+const warning = (msg) => ({result: validaton.WARNING, message: msg});
 
 function ndcValidator (ndc) {
     if (ndc==='')
@@ -20,7 +21,39 @@ function ndcValidator (ndc) {
             return error('NDC has more then 11 digits');
         return ok();
     }
-    return ok();
+    if (ndc.includes('-')) {
+        const regex_prefixed_ndc542 = /[a-z]+\d{5}-\d{4}-\d{2}\b/;
+        if (ndc.match(regex_prefixed_ndc542)) {
+            return warning('NDC with a prefix');
+        }
+        const regex_sufixed_ndc542 = /\b\d{5}-\d{4}-\d{2}[a-z]+/;
+        if (ndc.match(regex_sufixed_ndc542)) {
+            return warning('NDC with a sufix');
+        }
+        const regex_prefixword_ndc542 = /\w+ \d{5}-\d{4}-\d{2}\b/;
+        if (ndc.match(regex_prefixword_ndc542)) {
+            return error('NDC prefix is a separate word');
+        }
+        const regex_ndc542_sufixword = /\b\d{5}-\d{4}-\d{2} \w+/;
+        if (ndc.match(regex_ndc542_sufixword )!==null) {
+            return error('NDC sufix is a separate word');
+        }
+        const regex_ndc542 = /\b\d{5}-\d{4}-\d{2}\b/;
+        if (ndc.match(regex_ndc542)) {
+            return ok();
+        } else {
+            return error('Invalid NDC format, expected: 5-4-2');
+        }
+    }
+    const regex_ndcNumber_withPrefix = /[a-z]+\d{1,11}\b/;
+    if (ndc.match(regex_ndcNumber_withPrefix)) {
+        return warning('NDC with a prefix');
+    }
+    const regex_ndcNumber_withSufix = /\b\d{1,11}[a-z]+/;
+    if (ndc.match(regex_ndcNumber_withSufix)) {
+        return warning('NDC with a sufix');
+    }
+    return error('No valid NDC value');
 }
 
 module.exports = {validaton, ndcValidator};
--- fake-server/tests/ndc-validator-test.js
+++ fake-server/tests/ndc-validator-test.js
@@ -13,6 +13,10 @@ describe('ndcValidator', function() {
         ndcValidator(5).should.include(
             {result: validaton.OK});
     });
+    it('should return OK when ndc has 5-4-2 format', function() {
+        ndcValidator('12345-1234-12').should.include(
+            {result: validaton.OK});
+    });
     it('should return ERROR when ndc is zero', function() {
         ndcValidator(0).should.include(
             {result: validaton.ERROR, message: 'NDC is equal to 0'});
@@ -28,5 +32,41 @@ describe('ndcValidator', function() {
     it('should return ERROR when ndc is a number with 12 digits', function() {
         ndcValidator(123456789012).should.include(
             {result: validaton.ERROR, message:'NDC has more then 11 digits'});
+    });
+    it('should return ERROR when ndc has hypen and format is not 5-4-2', function() {
+        ndcValidator('12-345-123412').should.include(
+            {result: validaton.ERROR, message:'Invalid NDC format, expected: 5-4-2'});
+    });
+    it('should return ERROR when ndc (5-4-2) has prefix as a separate word', function() {
+        ndcValidator('prefix 12345-1234-12').should.include(
+            {result: validaton.ERROR, message:'NDC prefix is a separate word'});
+    });
+    it('should return ERROR when ndc (5-4-2) has sufix as a separate word', function() {
+        ndcValidator('12345-1234-12 sufix').should.include(
+            {result: validaton.ERROR, message:'NDC sufix is a separate word'});
+    });
+    it('should return WARNING when ndc is a number with prefix', function() {
+        ndcValidator('prefix1234123412').should.include(
+            {result: validaton.WARNING, message:'NDC with a prefix'});
+    });
+    it('should return WARNING when ndc is a number with sufix', function() {
+        ndcValidator('12345123412sufix').should.include(
+            {result: validaton.WARNING, message:'NDC with a sufix'});
+    });
+    it('should return WARNING when ndc is 5-4-2 and has a prefix', function() {
+        ndcValidator('prefix12345-1234-12').should.include(
+            {result: validaton.WARNING, message:'NDC with a prefix'});
+    });
+    it('should return WARNING when ndc is 5-4-2 and has a sufix', function() {
+        ndcValidator('12345-1234-12sufix').should.include(
+            {result: validaton.WARNING, message:'NDC with a sufix'});
+    });
+    it('should return ERROR when ndc is a word', function() {
+        ndcValidator('abc').should.include(
+            {result: validaton.ERROR, message:'No valid NDC value'});
+    });
+    it('should return ERROR when ndc has diffrent separator then hypen', function() {
+        ndcValidator('12345!1234=12').should.include(
+            {result: validaton.ERROR, message:'No valid NDC value'});
     });
 });
