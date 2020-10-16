097cf3cbaefed3764327b2282238e4bf40994a63
Bogdan Polak <bogdanpol@gmail.com>
2020-10-17 01:36:37 +0200

    Added validation to pharamacy processor

--- fake-server/src/pharmacy-processor.js
+++ fake-server/src/pharmacy-processor.js
@@ -1,5 +1,7 @@
 const fsp = require('fs').promises;
 const d3 = require("d3-dsv");
+const { report } = require('process');
+const validators = require("./validators");
 // add lowdb: https://github.com/typicode/lowdb 
 
 function parseCSV (csvtext) {
@@ -9,6 +11,74 @@ function parseCSV (csvtext) {
     });
 }
 
+function DetectMappings(data) {
+    let mappings = new Map()
+    mappings['Product Description'] =   {no: 1, name:'Description'};
+    mappings['NDC'] =                   {no: 2, name:'NDC'};
+    mappings['Average Invoice Price'] = {no: 3, name:'Purchase Cost'};
+    mappings['Shipped Qty'] =           {no: 4, name:'Volume'};
+    mappings['Total Ext Cost'] =        {no: 5, name:'Total Cost'};
+    mappings['Account Type'] =          {no: 6, name:'Account Type'};
+    return mappings;
+}
+
+const validaton = validators.validaton;
+
+function ValidateData(mappings, data) {
+    if (!data || data.length==0)
+        return [];
+    const header = data[0];
+    const columncount = header.length;
+    var columns = [];
+    header.forEach(
+        e => columns.push({}));
+    
+    for (var colidx=0; colidx<columncount; colidx++) {
+        columnName = header[colidx];
+        const mapping = mappings[columnName];
+        if (mapping) {
+            if (mapping.no === 2) {
+                mapping.idx = colidx;
+                mapping.validator = validators.ndcValidator; 
+            }
+            columns[colidx] = mapping;
+        }
+    }
+    var report = [];
+    for (let rowidx=1; rowidx<data.length; rowidx++) {
+        for (var colidx=0; colidx<columncount; colidx++) {
+            let row = data[rowidx];
+            if ('validator' in columns[colidx]) {
+                value = row[colidx];
+                result = columns[colidx].validator(value);
+                if (result.result === validaton.ERROR) {
+                    report.push({
+                        "type": "error",
+                        "message": result.message,
+                        "row": rowidx,
+                        "column": colidx
+                    });         
+                }
+            }
+        } 
+    }
+    return report;
+
+    const results = [
+        {
+            "type": "error",
+            "message": "NDC cannot be empty",
+            "row": 1,
+            "column": 5
+        },{
+            "type": "error",
+            "message": "Purchase cost is not a numeric value",
+            "row": 3,
+            "column": 8
+        }
+    ];
+    return results;
+}
 
 function processFile (file,uploadid) {
     // ---------------------------------------
@@ -25,6 +95,9 @@ function processFile (file,uploadid) {
     fsp.readFile(filename, "utf8")
         .then( (textdata) => parseCSV(textdata) )
         .then( (data) => {
+            mappings = DetectMappings(data);
+            results = ValidateData(mappings,data);
+
             setTimeout(() => console.log({
                 uploadid: uploadid,
                 started: Date(), 
